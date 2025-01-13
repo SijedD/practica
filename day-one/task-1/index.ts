228b491535dc10ff4036f8dd9d6b789c58022fcb
DANIL <149448448+SijedD@users.noreply.github.com>
2025-01-13 16:46:52 +0700

    day 1

--- day-one/task-1/index.ts
+++ day-one/task-1/index.ts
@@ -1,17 +1,18 @@
 function identicalLetters(str1: string, str2: string): string {
-
     let result = "";
+    let tempStr2 = str2;
 
     for (let i = 0; i < str1.length; i++) {
         const char = str1[i];
 
-        if (str2.indexOf(char) !== -1 && result.indexOf(char) === -1) {
+        if (tempStr2.indexOf(char) !== -1) {
             result += char;
+            tempStr2 = tempStr2.slice(tempStr2.indexOf(char) + 1);
         }
     }
 
     return result;
 }
 
-const result = identicalLetters("hello", "world");
+const result = identicalLetters("agsdffg", "agsdqffwe");
 console.log(result);
